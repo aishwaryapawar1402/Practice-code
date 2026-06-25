8ec0e0cf91403ba9af3a08e0b1cb50371ba79f87
Aishwarya Pawar <aishwaryapawar212@gmail.com>
2026-06-25 14:01:26 +0530

    my first pyspark code

---
+++ test.pyspark.py
@@ -0,0 +1,14 @@
+
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder \
+.appName("MyFirstSparkApp") \
+.getOrCreate()
+
+data = [("Laptop", 50000), ("Mobile", 20000), ("Tablet", 30000)]
+columns = ["Product", "Price"]
+df = spark.createDataFrame(data, columns)
+df.show()
+
+spark.stop()
+
